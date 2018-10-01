fb057a48c8384c7bffebadea3948574d30fd4e76
takahirohayasaka <hayasakatakahiro@hayasakatadahironoMacBook-puro.local>
2018-10-01 17:09:41 +0900

    アソシエーションを定義

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,2 +1,3 @@
 class Filter < ApplicationRecord
+  has_many :rooms
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,8 @@ class Room < ApplicationRecord
   belongs_to :amenity
   belongs_to :rule
   belongs_to :filter
+  has_many :reviews
+  has_many :reservations
   accepts_nested_attributes_for :amenity
   accepts_nested_attributes_for :rule
   accepts_nested_attributes_for :filter
