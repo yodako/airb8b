4d1211fb44014cdec35740e0ee6108e4751d9b2c
takahirohayasaka <hayasakatakahiro@hayasakatadahironoMacBook-puro.local>
2018-10-01 23:27:56 +0900

    enum変換
    Filterモデルの:category,:room_type,:bedroom_counter
    Roomモデルの:location,:currency
    をenumにより文字列に変化

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,4 +5,6 @@ class Room < ApplicationRecord
   accepts_nested_attributes_for :amenity
   accepts_nested_attributes_for :rule
   accepts_nested_attributes_for :filter
+  enum location: %w(アフガニスタン ニューヨーク インドネシア オーストラリア オランダ 象牙海岸 東京 マカオ 中国 ジャマイカ カザフスタン ヨルダン ハンガリー リビア バルバドス ベルギー ボリビア )
+  enum currency: [:JPY,:AED,:ARS,:AUD,:BGN,:BRL,:CAD,:CHF,:CLP,:CNY,:COP,:CRC,:CZK,:EUR,:GBP]
 end
