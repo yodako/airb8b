0b098bff85c3923482e2b2a928d69208cfd7006d
yodako <yoda@sana.local>
2018-10-02 16:58:45 +0900

    サムネイル表示修正
    # Conflicts:
    #	app/assets/stylesheets/_new.scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   root 'rooms#index'
   get 'new_introduction' => 'rooms#new_introduction'
   get 'serch_room' => 'rooms#serch'
+
   resources :rooms
+
+
 end
