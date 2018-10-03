1ed278991948d0681d7e94e9f4a61e9955c9b0ad
takahirohayasaka <hayasakatakahiro@hayasakatadahironoMacBook-puro.local>
2018-10-03 23:47:40 +0900

    予約確認画面 ルーティング設定(仮)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'rooms#index'
+  get 'reservation_confirm' => 'rooms#reservation_confirm'
   get 'new_introduction' => 'rooms#new_introduction'
   resources :rooms
 end
