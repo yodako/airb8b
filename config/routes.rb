8c789cb60e78d560535bf228e7b98dd092d5ae9b
yodako <yoda@sana.local>
2018-10-04 13:37:27 +0900

    Top画像変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations' }
-<<<<<<< HEAD
 
   root 'rooms#index'
   get 'new_introduction' => 'rooms#new_introduction'
   get 'serch_room' => 'rooms#serch'
 
   resources :rooms
-
-=======
-  
-  root 'rooms#index'
-  resources :rooms
-
-  get 'new_introduction' => 'rooms#new_introduction'
->>>>>>> fujimasa1031/master
 
 end
