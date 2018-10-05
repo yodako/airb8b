a79d1664785f1ab2b35668617ef8bd5d2b074fcf
takahirohayasaka <hayasakatakahiro@hayasakatadahironoMacBook-puro.local>
2018-10-05 13:58:24 +0900

    その他migrationファイルなど

--- db/migrate/20180930070725_create_reservations.rb
+++ db/migrate/20180930070725_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[5.2]
   def change
     create_table :reservations do |t|
-      t.references :user, null:false, foreign_key: true
-      t.references :room, null:false, foreign_key: true
+      t.references :user, null:false
+      t.references :room, null:false
       t.date :check_in
       t.date :check_out
       t.integer :occupancy
