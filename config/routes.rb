f76acacf9b5c3115045891da24f6be06e7929af5
yodako <yoda@sana.local>
2018-10-06 11:59:32 +0900

    Route.rb スペース削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: 'registrations' }  
+  devise_for :users, controllers: { registrations: 'registrations' }
   root 'rooms#index'
   resources :rooms do
     collection do
